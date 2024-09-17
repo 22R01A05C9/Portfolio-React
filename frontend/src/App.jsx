433df7f106d56de1f4cead33b1cd0cd353ad36e3
Ageera Saiteja <ageerasaiteja@gmail.com>
2024-09-17 11:46:12 +0530

    modified app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import cryptojs from 'crypto-js';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App(){
+  const encrypt = ()=>{
+    let text = "Hello World";
+    let encryptedText = cryptojs.AES.encrypt(text,import.meta.env.VITE_API_KEY).toString();
+    return encryptedText;
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>My First React App</h1>
+      <h3>React is awesome</h3>
+      <p>{encrypt()}</p>
+    </div>
   )
 }
 
-export default App
+export default App;
