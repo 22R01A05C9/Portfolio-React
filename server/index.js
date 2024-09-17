5f6c90949daaca498981f3a2ce22f4ed28a89994
Ageera Saiteja <ageerasaiteja@gmail.com>
2024-09-17 21:18:40 +0530

    added middleware to check website url and added url backend

--- server/index.js
+++ server/index.js
@@ -3,6 +3,15 @@ const ws = require("ws")
 const cors = require("cors")
 
 const app = express()
+app.use(function(req,res,next){
+    try{
+        decodeURIComponent(req.path)
+        next()
+    }catch(err){
+        //change after backend
+        res.redirect("https://saiteja.fun")
+    }
+})
 app.use(cors())
 app.use(express.json())
 
@@ -11,6 +20,7 @@ const wss = new ws.Server({ server: server })
 
 
 require("./files.js")(app)
+require("./url.js")(app)
 require("./sms.js")(wss)
 
 
