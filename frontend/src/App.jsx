cffc6f5bd64e9ad8d58d5f55b47d8474b327aa22
Ageera Saiteja <ageerasaiteja@gmail.com>
2024-09-17 17:01:23 +0530

    installed qrcode package

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,26 @@
 import cryptojs from 'crypto-js';
+import qrcode from "qrcode"
+import { useState, useRef } from 'react';
 
 function App(){
+  const [imgcode,setimgcode]=useState("")
+  const inpref = useRef()
   const encrypt = ()=>{
     let text = "Hello World";
     let encryptedText = cryptojs.AES.encrypt(text,import.meta.env.VITE_API_KEY).toString();
     return encryptedText;
   }
+  const generate =  ()=>{
+    qrcode.toDataURL(inpref.current.value).then((data)=>{
+      setimgcode(data)
+    })
+  }
   return (
     <div>
-      <h1>My First React App</h1>
-      <h3>React is awesome</h3>
-      <p>{encrypt()}</p>
+      <input type="text" id="data" ref={inpref}/>
+      <button onClick={generate}>Generate</button>
+      <img className='qrimage' src={imgcode} alt="" />
+      <a href={imgcode} download={"qrcode.png"}>Click To download</a>
     </div>
   )
 }
