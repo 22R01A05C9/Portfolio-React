71d9fa1d59eaf5474ea799ee37e01ff14e7a4a4b
Ageera Saiteja <ageerasaiteja@gmail.com>
2024-09-17 12:47:12 +0530

    moved files routes to new file

---
+++ backend/files.js
@@ -0,0 +1,91 @@
+const multer = require("multer")
+const fs = require("fs")
+const {MongoClient} = require("mongodb")
+module.exports = async function (app){
+    let connect = await MongoClient.connect("mongodb://localhost:27017")
+    async function getfilesrandomid(){
+        return new Promise(async (resolve)=>{
+            let files = db.collection("files")
+            let temp = await files.find().toArray()
+            let ids = []
+            temp.forEach((file)=>{
+                ids.push(parseInt(file.id))
+            })
+            let rand=1234
+            while(ids.includes(rand)){
+                rand = Math.floor(Math.random()*10000)
+                if(rand<1000)   rand+=1000;
+            }
+            resolve(rand)
+        })
+        
+    }
+
+    let db = connect.db("website")
+    const storage = multer.diskStorage({
+        destination: function (req, file, cb) {
+            getfilesrandomid().then((filerand)=>{
+                fs.mkdir("./filesdb/"+filerand,()=>{})
+                cb(null, './filesdb/'+filerand)
+            })
+        },
+        filename: function (req, file, cb) {
+            cb(null, file.originalname)
+        }
+    })
+    
+    const upload = multer({ storage: storage })
+
+    function removefiledata(fileid, filesdb){
+        setTimeout(()=>{
+            fs.rm("./filesdb/"+fileid, {recursive:true,force:true},()=>{})
+            filesdb.deleteOne({id:fileid})
+        }, 1000*60*120)
+    }
+    
+    async function addfiledatatodb(fileid, filename){
+        let db = connect.db("website")
+        let files = db.collection("files")
+        files.insertOne({id:fileid, filename:filename})
+        removefiledata(fileid,files)
+    }
+    
+    async function getfiledata(fileid){
+        let files = db.collection("files")
+        return await files.findOne({id:fileid})
+    }
+    
+    
+    app.post("/files/upload", upload.single("file"), function (req, res) {
+        try{
+            let fileid = req.file.destination.split("/")[2]
+            let filename = req.file.originalname
+            addfiledatatodb(fileid, filename)
+            res.json({status:true,id:fileid})
+        }catch(err){
+            res.json({status:false})
+        }
+    })
+    
+    app.get("/files/download/:id",(req,res)=>{
+        let id = req.params.id;
+        getfiledata(id).then((data)=>{
+            if(data){
+                res.download("./filesdb/"+id+"/"+data.filename)
+            }else{
+                res.send("cannot find file")
+            }
+        })
+    })
+    
+    app.post("/files/download",(req,res)=>{
+        let id = req.body.id;
+        getfiledata(id).then((data)=>{
+            if(data){
+                res.json({status:true, redirect:"/files/download/"+id})
+            }else{
+                res.json({status:false})
+            }
+        })
+    })
+}
