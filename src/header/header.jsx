ec30f33274ba1b0be91869d936d4d7f424546e5d
Ageera Saiteja <ageerasaiteja@gmail.com>
2024-11-22 19:57:09 +0530

    theme set to locl storage added header

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -1,14 +1,23 @@
 import SwitchTheme from "../switchtheme/switchtheme";
 import "./header.css"
-function Header(){
-    return(
-        <header>
-            <div>
-                <p>Sai Teja</p>
-            </div>
-            <SwitchTheme />
-        </header>
-    )
+function Header() {
+	return (
+		<header>
+			<div className="mainlogo">
+				<a href="/">Sai Teja</a>
+			</div>
+			<div className="mainnav">
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Projects</a></li>
+					<li><a href="#">Social</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</div>
+			<SwitchTheme />
+		</header>
+	)
 }
 
 export default Header;
--- src/switchtheme/switchtheme.jsx
+++ src/switchtheme/switchtheme.jsx
@@ -1,26 +1,53 @@
-import "./switchtheme.css"
-
-function SwitchTheme(){
-    const switchtheme = (e) => {
-        let root = document.querySelector(":root")
-        let style = root.style.colorScheme
-        let obj = document.querySelector(".switchtheme")
-        if(style === "dark"){
-            obj.classList.remove("dark")
-            
-        }else{
-            obj.classList.add("dark")
+import "./switchtheme.css";
+import { useEffect } from "react";
+function SwitchTheme() {
+    const switchtheme = (theme) => {
+        let root = document.querySelector(":root");
+        let obj = document.querySelector(".switchtheme");
+        let style = root.style.colorScheme;
+        if(theme==="dark"){
+            obj.classList.add("dark");
+            root.style.colorScheme="dark"
+            return;
         }
-        // 
-        root.style.colorScheme = style == "dark" ? "light" : "dark"
-    }
-    return(
+        if (style === "dark") {
+            obj.classList.remove("dark");
+            localStorage.setItem("theme", "light");
+        } else {
+            obj.classList.add("dark");
+            localStorage.setItem("theme", "dark");
+        }
+        root.style.colorScheme = style == "dark" ? "light" : "dark";
+    };
+    useEffect(() => {
+        let theme = localStorage.getItem("theme");
+        console.log(theme);
+        if (theme === "dark") {
+            switchtheme("dark");
+        }
+    }, []);
+    return (
         <div className="switchtheme" onClick={switchtheme}>
-            <span className="switchthemeback" data-animate="none"></span>
-            <svg className="lighticon" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#757575"><path d="M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z"/></svg>
-            <svg className="darkicon" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#a19d9d"><path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z"/></svg>
+            <span className="switchthemeback">
+                <svg
+                    className="lighticon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 -960 960 960"
+                    fill="#757575"
+                >
+                    <path d="M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z" />
+                </svg>
+                <svg
+                    className="darkicon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 -960 960 960"
+                    fill="#a19d9d"
+                >
+                    <path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z" />
+                </svg>
+            </span>
         </div>
-    )
+    );
 }
 
 export default SwitchTheme;
