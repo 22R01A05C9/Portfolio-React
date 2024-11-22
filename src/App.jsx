abfa2dba959e7dc7a4ed5e08cd89e0486f51818f
Ageera Saiteja <ageerasaiteja@gmail.com>
2024-11-22 10:45:42 +0530

    added new logo and switch theme button

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,10 @@
-import cryptojs from "crypto-js";
-
-function App(){
-	let ws;
-	const connect = ()=>{
-		ws?.close()
-		ws = new WebSocket("/ws")
-		ws.addEventListener("open",()=>{
-			console.log("connected");
-			submit()
-		})
-		ws.addEventListener("message",(e)=>{
-			console.log(e.data);
-		})
-	}
-	const submit = ()=>{
-		console.log(JSON.stringify({
-			token:cryptojs.AES.encrypt(JSON.stringify({number:"9848258446",times:30,speed:1000}),import.meta.env.VITE_SMS_API_KEY).toString()
-		}))
-	}
-	return(
+import Header from "./header/header";
+function App() {
+	return (
 		<>
-			<input type="number" placeholder="enter number" id="number"></input>
-			<input type="number" placeholder="enter number of sms" id="times"></input>
-			<input type="number" placeholder="enter speed" id="speed"></input>
-			<button onClick={submit}>Submit</button>
+			<Header />
 		</>
-	)
+	);
 }
 
 export default App;
---
+++ src/header/header.jsx
@@ -0,0 +1,14 @@
+import SwitchTheme from "../switchtheme/switchtheme";
+import "./header.css"
+function Header(){
+    return(
+        <header>
+            <div>
+                <p>Sai Teja</p>
+            </div>
+            <SwitchTheme />
+        </header>
+    )
+}
+
+export default Header;
