24df2dd3442f342c45b62b1306b6569b4c75e574
Ageera Saiteja <ageerasaiteja@gmail.com>
2025-02-02 12:19:14 +0530

    added validation and sending api request to sms backend

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import Mainpage from "./pages/mainpage/mainpage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Loading from "./pages/loadingpage/loading";
 import { lazy, Suspense } from "react";
+import {ToastContainer} from "react-toastify"
+import "./App.css"
+
 
 const Sms = lazy(()=>import("./pages/smspage/sms"))
+
+
 
 function App() {
 
 	return (
 		<Suspense fallback={<Loading/>}>
+			<ToastContainer />
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<Mainpage />} />
--- src/pages/smspage/sms.jsx
+++ src/pages/smspage/sms.jsx
@@ -2,23 +2,39 @@ import Header from "../../components/header/header"
 import "./sms.css"
 import Smsradio from "../../components/smsradio/smsradio"
 import { useRef } from "react"
-import { ToastContainer, toast } from "react-toastify"
+import {  toast } from "react-toastify"
 import 'react-toastify/dist/ReactToastify.css'
-
-
+import { AES } from "crypto-js"
 
 function Sms(){
+    const submit=(document)=>{
+        let inputs = document.querySelector(".userinputs")
+        let number = inputs.querySelector(".number input").value
+        let times = inputs.querySelector(".times input").value
+        let speed = inputs.querySelector(".speed .radio .active .radiobtn").textContent
+        if(speed === "Slow") speed = 1500
+        else if(speed === "Medium") speed = 1000
+        else speed = 500
+        let data = {number:number, times:times, speed:speed}
+        let token = AES.encrypt(JSON.stringify(data) , import.meta.env.VITE_SMS_API_KEY).toString()
+        let socket = new WebSocket("http://localhost:5000")
+        socket.addEventListener("open",()=>{
+            socket.send(JSON.stringify({token:token}))
+        })
+    }
     const inp = (e)=>{
         if(e.target.value.trim() === ""){
             e.target.parentNode.classList.remove("filled")
         }else{
             e.target.parentNode.classList.add("filled")
         }
+        e.target.parentNode.classList.remove("error")
     }
     const verify = ()=>{
         let document = smsref.current
         let number = document.querySelector(".number")
-        if(number.querySelector("input").value === ""){
+        let times = document.querySelector(".times")
+        if(number.querySelector("input").value.trim() === ""){
             toast.error("Please Enter Number!!",{
                 theme:(localStorage.getItem("theme") ? localStorage.getItem("theme") : "dark"),
                 autoClose: 2000,
@@ -26,25 +42,56 @@ function Sms(){
                 draggable: true
             })
             number.classList.add("error")
+            return ;
+        }else if(number.querySelector("input").value.trim().length !== 10){
+            toast.error("Please Enter Valid Number!!",{
+                theme:(localStorage.getItem("theme") ? localStorage.getItem("theme") : "dark"),
+                autoClose: 2000,
+                closeOnClick: true,
+                draggable: true
+            })
+            number.classList.add("error")
+            return ;
         }
-
-
+        if(times.querySelector("input").value.trim() === ""){
+            toast.error("Please Enter TImes!!",{
+                theme:(localStorage.getItem("theme") ? localStorage.getItem("theme") : "dark"),
+                autoClose: 2000,
+                closeOnClick: true,
+                draggable: true
+            })
+            times.classList.add("error")
+            return ;
+        }else if(parseInt(times.querySelector("input").value) > 150){
+            toast.error("Please Enter Less Than 150 Times",{
+                theme:(localStorage.getItem("theme") ? localStorage.getItem("theme") : "dark"),
+                autoClose: 2000,
+                closeOnClick: true,
+                draggable: true
+            })
+            times.classList.add("error")
+            return ;
+        }
+        submit(document)
     }
     const smsref = useRef()
     return(
         <div className="sms" ref={smsref}>
-            <ToastContainer />
             <Header ext="/" />
             <div className="smsbody">
                 <h2>SMS Bomber</h2>
                 <p className="sinfo">Please Enter The Below Details To Start Bombing</p>
                 <div className="userinputs">
                     <div className="number">
-                        <input type="number" id="number" onInput={inp}/>
+                        <input onInput={inp} type="number" id="number" />
                         <label htmlFor="number">Number</label>
                     </div>
                     <div className="times">
-                        <input type="number" id="times" onInput={inp}/>
+                        <input type="number" id="times" onInput={inp} onKeyDown={(e)=>{
+                            if(e.key === "Enter"){
+                                verify()
+                            }
+                        }}/>
                         <label htmlFor="times">SMS's</label>
                     </div>
                     <div className="speed">
