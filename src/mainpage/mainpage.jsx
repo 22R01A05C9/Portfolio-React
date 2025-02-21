e08191201c29e8445f46f4a47623b8522b8084a3
Ageera Saiteja <ageerasaiteja@gmail.com>
2025-02-21 19:43:12 +0530

    added contact page need to work on cpu usage

--- src/mainpage/mainpage.jsx
+++ src/mainpage/mainpage.jsx
@@ -2,7 +2,7 @@ import Header from "../components/header/header"
 import { useEffect } from "react"
 import Home from "./homepage/homepage"
 import Projects from "./projectspage/projects"
-
+import Contact from "./contactpage/contact"
 
 function Mainpage(){
     useEffect(()=>{
@@ -24,6 +24,7 @@ function Mainpage(){
         <>
             <Header ext="#" active="home"/>
 			<Projects />
+			<Contact />
         </>
     )
 }
