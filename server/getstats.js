50c813ec61d3a4f61a47307663ad8d2aeb896a52
Ageera Saiteja <ageerasaiteja@gmail.com>
2025-02-23 22:17:29 +0530

    added sms retrieval

--- server/getstats.js
+++ server/getstats.js
@@ -5,11 +5,13 @@ module.exports = function (app) {
     const getstats = async (req,res)=>{
         if(req.query.pass !== process.env.PASS) 
             return res.json({error:true,message:"Not Authorized"})
+        let collection = req.params.app
+        if (collection == null) collection = "stats"
         let conn = await client.connect();
-        let stats = conn.db("website").collection("stats");
+        let stats = conn.db("website").collection(collection);
         let data = await stats.find({},{"projection":{_id:0}}).toArray();
         res.json(data)
     }
 
-    app.get("/stats",getstats)
+    app.get("/stats/:app",getstats)
 }
